dd948c264acd67392d887b691542b23273b268b9
Дмитрий Шведов <shvedovd007@yandex.ru>
2024-05-28 13:03:57 +0500

    add bg on breakpoint, debug slider

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  autoHeight: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: true,
+  keyboard: true,
+});
